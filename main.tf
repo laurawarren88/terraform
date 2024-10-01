dd58143e86bbe5b0e2be73afc54df20d3c04393a
laurawarren88 <laura@enterpriseautomation.co.uk>
2024-10-01 09:43:19 +0100

    First Commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "vsphere" {
+    user                    = var.vcenter_username
+    password                = var.vcenter_password 
+    vsphere_server          = var.vcenter_server 
+    allow_unverified_ssl    = true
+}
+
+module "create_vms" {
+    source = "./modules/create_vms"
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "vcenter_username" {
+    default = "laura@easlab.co.uk"
+}
+
+variable "vcenter_password" {
+    default =  "Renegade187!"
+}
+
+variable "vcenter_server" {
+    default = "vcenter.easlab.co.uk"
+}
+
